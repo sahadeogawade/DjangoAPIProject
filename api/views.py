726ddbd2422bad2e8be09dc9d6aaa1ab32d3bd1c
Sahadeo Gawde <sahadeo2494@gmail.com>
2020-02-28 21:06:48 +0530

    get data on basis id by using request data

--- api/views.py
+++ api/views.py
@@ -14,10 +14,56 @@ import logging
 
 class NoteAPIView(APIView):
 
+  def get_object(self, title):
+    try: 
+      return Note.objects.get(title=title)
+
+    except Note.DoesNotExist:
+      # return JsonResponse(data={},status=404)
+      return ''
+
   def get(self, request):
-    notes = Note.objects.all()
-    serializer = NoteSerializer(notes, many= True)
-    return JsonResponse(serializer.data, safe= False)
+    # title = request.GET.get('request.data.title')
+    title = request.data['title']
+    data = {}
+    # title = '' if request.data['title'] == null else request.data['title']
+    print("***request stream",request.stream) 
+    print(request.data) #request body
+    if title: #search by title
+      note = self.get_object(title)
+      print("***Records",note)
+      if note:
+      # if note.is_valid():
+        serializer = NoteSerializer(note)
+        data['results'] = serializer.data
+        data['data_count'] = Note.objects.filter(title=title).count()
+        # if serializer.is_valid():
+        print("**Serializer",Note.objects.filter(title=title).count())
+        # return JsonResponse(serializer.data, safe= False)
+        return JsonResponse(data,safe=False)
+      # print("***Errors",serializer.errors)
+      data['data_count'] = 0
+      data['results'] = []
+      return JsonResponse(data,status=404)
+      # else:
+      #   return HttpResponse(status=404)
+      # try:
+      #   note = Note.objects.get(title= title)
+      #   print("Records exist",note)
+      # except Note.DoesNotExist:
+      #   print("*Note exist")
+      #   return HttpResponse(status=404)
+    else: #return all 
+      notes = Note.objects.all()
+      data['data_count'] = Note.objects.all().count()
+      print("****result",notes)
+      serializer = NoteSerializer(notes, many= True)
+      data['results'] = serializer.data
+      return JsonResponse(data, safe= False)
+    # if note:
+    #   serializer = NoteSerializer(note)
+    #   print("**Serializer",serializer.data)
+    #   return JsonResponse(serializer.data, safe= False)
 
   def post(self, request):
     data = JSONParser().parse(request)
@@ -37,13 +83,17 @@ class NoteDetailAPIView(APIView):
       return Note.objects.get(id=id)
 
     except Note.DoesNotExist:
-      return HttpResponse(status=404)
+      # return HttpResponse(status=404)
+      return ''
 
   def get(self, request, id):
-    note = self.get_object(id)
-    serializer = NoteSerializer(note)
-    return JsonResponse(serializer.data)
-
+    note_id = request.data['id']
+    note = self.get_object(note_id)
+    data = {}
+    if note:
+      serializer = NoteSerializer(note)
+      return JsonResponse(serializer.data)
+    return JsonResponse(data, status=404)
   
   def put(self, request, id):
     note = self.get_object(id)
@@ -51,7 +101,8 @@ class NoteDetailAPIView(APIView):
     if serializer.is_valid():
       serializer.save()
       return JsonResponse(serializer.data)
-    return JsonResponse(serializer.errors, status=400,data={"message":"Data doesn't exist"})
+    print("***Error in put",serializer.errors)
+    return JsonResponse(serializer.errors, status=400)
 
   def delete(self, request, id):
     note = self.get_object(id)
